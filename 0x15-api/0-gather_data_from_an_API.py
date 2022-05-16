631742aa96155da13b0f0f1e74d12fa089047ccb
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-16 13:36:57 -0700

    casting to int??????

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,9 +11,9 @@ from sys import argv
 if __name__ == "__main__":
 
     todos = requests.get("https://jsonplaceholder.typicode.com/\
-todos?userId={}".format(argv[1]))
+todos?userId={}".format(int(argv[1])))
     user = requests.get("https://jsonplaceholder.typicode.com\
-/users/{}".format(argv[1]))
+/users/{}".format(int(argv[1])))
     todos = todos.json()
     user = user.json()
     totalTasks = 0
