9284c09596ce8ef3ae8fe66bc54db21b26bb5d1d
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-16 12:21:21 -0700

    fixed codestyle

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,51 +3,56 @@
 using the https://jsonplaceholder.typicode.com/ API with the requests module,
 saves a todo_all_employees.json with specific content of the requests maded
 """
-import requests
-from sys import argv
+
+
 import json as js
 import os
+import requests
+from sys import argv
 
 
-def lazy(n, dictio):
-    """
-    automatize the task in one single function
-    """
-    todos = requests.get(f'https://jsonplaceholder.typicode.com/\
-todos?userId={n}')
-    user = requests.get(f'https://jsonplaceholder.typicode.com/users/{n}')
-    todos = todos.json()
-    user = user.json()
-    if len(user) == 0 or len(todos) == 0:
-        return None
+if __name__ == "__main__":
 
-    key = f"{n}"
-    if dictio is None:
-        bigDict = {key: []}
-    else:
-        bigDict = dictio
-        bigDict[f"{n}"] = []
+    def lazy(n, dictio):
+        """
+        automatize the task in one single function
+        """
+        todos = requests.get("https://jsonplaceholder.typicode.com/\
+todos?userId={}".format(n))
+        user = requests.get("https://jsonplaceholder.typicode.com/users/{}\
+".format(n))
+        todos = todos.json()
+        user = user.json()
+        if len(user) == 0 or len(todos) == 0:
+            return None
 
-    if not os.path.exists(f"todo_all_employees.json"):
-        for info in todos:
-            littleDict = {}
-            littleDict["username"] = user.get('username')
-            littleDict["task"] = info.get('title')
-            littleDict["completed"] = info.get('completed')
-            bigDict[key].append(littleDict)
-    return bigDict
+        key = "{}".format(n)
+        if dictio is None:
+            bigDict = {key: []}
+        else:
+            bigDict = dictio
+            bigDict["{}".format(n)] = []
+
+        if not os.path.exists("todo_all_employees.json"):
+            for info in todos:
+                littleDict = {}
+                littleDict["username"] = user.get('username')
+                littleDict["task"] = info.get('title')
+                littleDict["completed"] = info.get('completed')
+                bigDict[key].append(littleDict)
+        return bigDict
 
 
-flag = False
-for i in range(10):
-    if flag is False:
-        flag = True
-        check = lazy(i + 1, None)
-        if check is not None:
-            FinalDict = check
-    else:
-        check = lazy(i + 1, FinalDict)
-        if check is not None:
-            FinalDict = check
-with open(f"todo_all_employees.json", 'a') as f:
-    js.dump(FinalDict, f)
+    flag = False
+    for i in range(10):
+        if flag is False:
+            flag = True
+            check = lazy(i + 1, None)
+            if check is not None:
+                FinalDict = check
+        else:
+            check = lazy(i + 1, FinalDict)
+            if check is not None:
+                FinalDict = check
+    with open("todo_all_employees.json", 'a') as f:
+        js.dump(FinalDict, f)
