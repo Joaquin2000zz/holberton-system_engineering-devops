3eb24b3cbf6f205605774a1302836c3abcd347e3
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-16 12:16:52 -0700

    fixed code style

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,26 +3,32 @@
 using the https://jsonplaceholder.typicode.com/ API with the requests module,
 saves a <USERID>.json with specific content of the requests maded
 """
-import requests
-from sys import argv
+
+
 import json as js
 import os
+import requests
+from sys import argv
 
-todos = requests.get(f'https://jsonplaceholder.typicode.com/\
-todos?userId={argv[1]}')
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-todos = todos.json()
-user = user.json()
 
-key = f"{argv[1]}"
-bigDict = {key: []}
+if __name__ == "__main__":
 
-if not os.path.exists(f"{user.get('id')}.json"):
-    for info in todos:
-        littleDict = {}
-        littleDict["task"] = info.get('title')
-        littleDict["completed"] = info.get('completed')
-        littleDict["username"] = user.get('username')
-        bigDict[key].append(littleDict)
-    with open(f"{user.get('id')}.json", 'a') as f:
-        js.dump(bigDict, f)
+    todos = requests.get("https://jsonplaceholder.typicode.com/\
+todos?userId={}".format(argv[1]))
+    user = requests.get("https://jsonplaceholder.typicode.com/users/\
+{}".format(argv[1]))
+    todos = todos.json()
+    user = user.json()
+
+    key = "{}".format(argv[1])
+    bigDict = {key: []}
+
+    if not os.path.exists("{}.json".format(user.get('id'))):
+        for info in todos:
+            littleDict = {}
+            littleDict["task"] = info.get('title')
+            littleDict["completed"] = info.get('completed')
+            littleDict["username"] = user.get('username')
+            bigDict[key].append(littleDict)
+        with open("{}.json".format(user.get('id')), 'a') as f:
+            js.dump(bigDict, f)
