77f9ca04ae4680f07b1934fc3f63843d3794d392
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-16 11:54:21 -0700

    fixing pycodestyle

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,22 +1,30 @@
+#!/usr/bin/python3
 """
 using the https://jsonplaceholder.typicode.com/ API with the requests module
 """
+
+
 import requests
 from sys import argv
-todos = requests.get(f'https://jsonplaceholder.typicode.com/\
-todos?userId={argv[1]}')
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
-todos = todos.json()
-user = user.json()
-totalTasks = 0
-completed = 0
-completedList = []
-for info in todos:
-    totalTasks += 1
-    if info.get('completed') is True:
-        completed += 1
-        completedList.append(info.get('title'))
-print(f"Employee {user.get('name')} is done with tasks({completed}/\
-{totalTasks}):")
-for line in completedList:
-    print(f"\t{line}")
+
+
+if __name__ == "__main__":
+
+    todos = requests.get("https://jsonplaceholder.typicode.com/\
+todos?userId={}".format(argv[1]))
+    user = requests.get("https://jsonplaceholder.typicode.com\
+/users/{}".argv[1])
+    todos = todos.json()
+    user = user.json()
+    totalTasks = 0
+    completed = 0
+    completedList = []
+    for info in todos:
+        totalTasks += 1
+        if info.get('completed') is True:
+            completed += 1
+            completedList.append(info.get('title'))
+    print("Employee {} is done with tasks({}/\
+{totalTasks}):".format(user.get('name'), completed))
+    for line in completedList:
+        print("\t{}".format(line))
