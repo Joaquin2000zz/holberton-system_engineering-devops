7917eb976d6c7f5c29dd3b01dd74f0f2af2eb9d3
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-16 12:06:39 -0700

    fixed code style

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,23 +3,28 @@
 using the https://jsonplaceholder.typicode.com/ API with the requests module,
 saves a <USERID>.csv with specific content of the requests maded
 """
-import requests
-from sys import argv
+
+
 import csv
 import os
+import requests
+from sys import argv
 
-todos = requests.get(f'https://jsonplaceholder.typicode.com/\
-todos?userId={argv[1]}')
-user = requests.get(f'https://jsonplaceholder.typicode.com/users/{argv[1]}')
+
+todos = requests.get("https://jsonplaceholder.typicode.com/\
+todos?userId={}".format(argv[1]))
+user = requests.get("https://jsonplaceholder.typicode.com/users/{}\
+".format(argv[1]))
 todos = todos.json()
 user = user.json()
 
-completedList = [f"{argv[1]}", f"{user.get('username')}", "", ""]
+completedList = ["{}".format(argv[1]), "{}".format(user.get('username\
+')), "", ""]
 
-if not os.path.exists(f"{user.get('id')}.csv"):
+if not os.path.exists("{}.csv".format(user.get('id'))):
     for info in todos:
         completedList[2] = info.get('completed')
         completedList[3] = info.get('title')
-        with open(f"{user.get('id')}.csv", 'a') as f:
+        with open("{}.csv".format(user.get('id')), 'a') as f:
             writer = csv.writer(f, quoting=csv.QUOTE_ALL)
             writer.writerow(completedList)
