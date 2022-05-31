28fde7b3bb8b3264d8b7beaa3728f6e9ea3ced60
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-31 18:21:55 +0000

    now using status-code's response to check if it's a valid request or not

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API and
     returns the number of subscribers
     """
-    response = requests.get('https://reddit.com/r/{}/about.jsoni\
+    response = requests.get('https://reddit.com/r/{}/about.json\
 '.format(subreddit), headers = {'User-agent': 'botardo'})
     return response.json().get('data').get('subscribers\
-') if response.json().get('data').get('subscribers') is not None else 0
+') if response.status_code != 404 else 0
