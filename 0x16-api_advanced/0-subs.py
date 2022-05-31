da8bf64d0d072d732e821e172c6529b6ca70a79b
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-31 18:09:29 +0000

    function that queries the Reddit API and returns the number of subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""module which contains a function to bring information of Reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    function that queries the Reddit API and
+    returns the number of subscribers
+    """
+    response = requests.get('https://reddit.com/r/{}/about.json'.format(subreddit),
+                            headers = {'User-agent': 'botardo'})
+    return response.json().get('data').get('subscribers\
+') if response.json().get('data').get('subscribers') is not None else 0
