35e4cdfa9164572a88a8f4c732e3cb44734102bf
Joaquin2000zz <joaquin2000zz@gmail.com>
2022-05-31 19:11:46 +0000

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""module which contains a function to bring information of Reddit API"""
+import requests
+import json
+
+
+def top_ten(subreddit):
+    """
+    function that queries the Reddit API and
+    returns the titles of the first hot posts of a given subreddit
+    """
+    response = requests.get('https://reddit.com/r/{}/hot.json\
+'.format(subreddit), headers={'User-agent': 'botardo'})
+    dataList = response.json().get('data').get('children\
+') if response.status_code != 404 else 0
+    i = 0
+    for item in dataList:
+        print(item.get('data').get('title')) if i < 11 else None
+        i += 1
